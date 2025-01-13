2c96d73587e6ab7c07ef5d9566d0b61716192889
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2025-01-13 20:54:30 +0100

    add guest list and initialize it in constructor

--- src/models/Room.java
+++ src/models/Room.java
@@ -1,16 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Room {
     private int roomNumber;
     private int capacity;
     private boolean hasBathroom;
     private boolean isAvailable;
+    private List<Guest> guests;
 
     public Room(int roomNumber, int capacity, boolean hasBathroom, boolean isAvailable) {
         this.roomNumber = roomNumber;
         this.capacity = capacity;
         this.hasBathroom = hasBathroom;
         this.isAvailable = isAvailable;
+        this.guests = new ArrayList<>();
     }
 
     public int getRoomNumber() {
