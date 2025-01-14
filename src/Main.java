c8ba5760402d15b2a5e60a634adb544c6b64fa23
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2025-01-14 20:00:14 +0100

    add feature to cancel adding guests in any time

--- src/Main.java
+++ src/Main.java
@@ -47,75 +47,113 @@ public class Main {
                     break;
 
                 case 3:
-                    int roomNumberToReserve;
-
-                    // exception handling for invalid input
                     while (true) {
-                        try {
-                            System.out.print("Podaj numer pokoju do rezerwacji: ");
-                            roomNumberToReserve = scanner.nextInt();
-                            scanner.nextLine();
-                            break;
-                        } catch (InputMismatchException e) {
-                            System.out.println("Proszę wprowadzić poprawny numer pokoju.");
-                            scanner.next();
-                        }
-                    }
+                        int roomNumberToReserve;
 
-                    // list of guests
-                    List<Guest> guests = new ArrayList<>();
-                    int numberOfGuests;
-                    // exception handling for invalid input
-                    while (true) {
-                        try {
-                            System.out.print("Podaj liczbę gości (maksymalnie 4): ");
-                            numberOfGuests = scanner.nextInt();
-                            scanner.nextLine();
-                            if (numberOfGuests > 4) {
-                                System.out.println(
-                                        "Maksymalna liczba gości w pokoju to 4. Proszę wprowadzić poprawną liczbę.");
-                            } else {
-                                break;
-                            }
-                        } catch (InputMismatchException e) {
-                            System.out.println("Proszę wprowadzić poprawną liczbę gości.");
-                            scanner.next();
-                        }
-                    }
-                    // loop to add new guests
-                    for (int i = 0; i < numberOfGuests; i++) {
-                        System.out.print("Podaj imię gościa: ");
-                        String firstName = scanner.nextLine();
-                        System.out.print("Podaj nazwisko gościa: ");
-                        String lastName = scanner.nextLine();
-                        LocalDate birthDay;
+                        // exception handling for invalid input
                         while (true) {
-                            // exception handling for invalid date format
                             try {
-                                System.out.print("Podaj datę urodzenia gościa (YYYY-MM-DD): ");
-                                birthDay = LocalDate.parse(scanner.nextLine());
+                                System.out.print("Podaj numer pokoju do rezerwacji (lub 0, aby anulować): ");
+                                roomNumberToReserve = scanner.nextInt();
+                                scanner.nextLine();
+                                if (roomNumberToReserve == 0) {
+                                    System.out.println("Rezerwacja anulowana. Rozpocznij od nowa.");
+                                    break;
+                                }
                                 break;
-                            } catch (DateTimeParseException e) {
-                                System.out.println("Proszę wprowadzić datę w formacie YYYY-MM-DD.");
+                            } catch (InputMismatchException e) {
+                                System.out.println("Proszę wprowadzić poprawny numer pokoju.");
+                                scanner.next();
                             }
                         }
-                        guests.add(new Guest(firstName, lastName, birthDay));
-                    }
 
-                    // exception handling for reserveRoom method
-
-                    try {
-                        if (userService.reserveRoom(roomNumberToReserve, guests)) {
-                            System.out.println("Pokój numer " + roomNumberToReserve + " został zarezerwowany.");
-                        } else {
-                            System.out.println("Pokój numer " + roomNumberToReserve
-                                    + " jest zajęty lub brak pełnoletniego gościa.");
+                        if (roomNumberToReserve == 0) {
+                            break;
                         }
-                    } catch (Exception e) {
-                        System.out.println("Wystąpił błąd podczas rezerwacji pokoju: " + e.getMessage());
+
+                        // list of guests
+                        List<Guest> guests = new ArrayList<>();
+                        int numberOfGuests;
+
+                        // exception handling for invalid input
+                        while (true) {
+                            try {
+                                System.out.print("Podaj liczbę gości (maksymalnie 4, lub 0, aby anulować): ");
+                                numberOfGuests = scanner.nextInt();
+                                scanner.nextLine();
+                                if (numberOfGuests == 0) {
+                                    System.out.println("Rezerwacja anulowana. Rozpocznij od nowa.");
+                                    break;
+                                }
+                                if (numberOfGuests > 4) {
+                                    System.out.println(
+                                            "Maksymalna liczba gości w pokoju to 4. Proszę wprowadzić poprawną liczbę.");
+                                } else {
+                                    break;
+                                }
+                            } catch (InputMismatchException e) {
+                                System.out.println("Proszę wprowadzić poprawną liczbę gości.");
+                                scanner.next();
+                            }
+                        }
+
+                        if (numberOfGuests == 0) {
+                            break;
+                        }
+
+                        // loop to get guest details
+                        for (int i = 0; i < numberOfGuests; i++) {
+                            System.out.print("Podaj imię gościa (lub 0, aby anulować): ");
+                            String firstName = scanner.nextLine();
+                            if (firstName.equals("0")) {
+                                System.out.println("Rezerwacja anulowana. Rozpocznij od nowa.");
+                                break;
+                            }
+                            System.out.print("Podaj nazwisko gościa (lub 0, aby anulować): ");
+                            String lastName = scanner.nextLine();
+                            if (lastName.equals("0")) {
+                                System.out.println("Rezerwacja anulowana. Rozpocznij od nowa.");
+                                break;
+                            }
+                            LocalDate birthDay = null;
+
+                            // exception handling for invalid input
+                            while (true) {
+                                try {
+                                    System.out.print("Podaj datę urodzenia gościa (YYYY-MM-DD, lub 0, aby anulować): ");
+                                    String input = scanner.nextLine();
+                                    if (input.equals("0")) {
+                                        System.out.println("Rezerwacja anulowana. Rozpocznij od nowa.");
+                                        break;
+                                    }
+                                    birthDay = LocalDate.parse(input);
+                                    break;
+                                } catch (DateTimeParseException e) {
+                                    System.out.println("Proszę wprowadzić datę w formacie YYYY-MM-DD.");
+                                }
+                            }
+
+                            if (birthDay == null) {
+                                break;
+                            }
+
+                            guests.add(new Guest(firstName, lastName, birthDay));
+                        }
+
+                        // exception handling for reserveRoom method
+                        try {
+                            if (userService.reserveRoom(roomNumberToReserve, guests)) {
+                                System.out.println("Pokój numer " + roomNumberToReserve + " został zarezerwowany.");
+                            } else {
+                                System.out.println("Pokój numer " + roomNumberToReserve
+                                        + " jest zajęty lub brak pełnoletniego gościa.");
+                            }
+                        } catch (Exception e) {
+                            System.out.println("Wystąpił błąd podczas rezerwacji pokoju: " + e.getMessage());
+                        }
+                        break;
                     }
                     break;
-
                 case 4:
                     int roomNumberToRelease;
 
@@ -124,11 +162,11 @@ public class Main {
                         try {
                             System.out.print("Podaj numer pokoju który chcesz zwolnić: ");
                             roomNumberToRelease = scanner.nextInt();
-                            scanner.nextLine(); // Consume newline
+                            scanner.nextLine();
                             break;
                         } catch (InputMismatchException e) {
                             System.out.println("Proszę wprowadzić poprawny numer pokoju.");
-                            scanner.next(); // Clear the invalid input
+                            scanner.next();
                         }
                     }
 
